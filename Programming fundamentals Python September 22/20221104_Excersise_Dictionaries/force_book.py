cdd3e8185ad4fade832b710b962cd02c977a30f7
chebishev <atanas.chebishev@gmail.com>
2022-11-10 16:02:34 +0200

    the force is strong with this one

--- Programming fundamentals Python September 22/20221104_Excersise_Dictionaries/force_book.py
+++ Programming fundamentals Python September 22/20221104_Excersise_Dictionaries/force_book.py
@@ -4,6 +4,7 @@ def check_existing_side(dictionary, side):
 
 
 force_book = {}
+force_user_exists = False
 
 while True:
 
@@ -13,18 +14,26 @@ while True:
 
     if " | " in command:
         force_side, force_user = command.split(" | ")
-        if force_side not in force_book.keys() and force_user not in force_book.values():
-            if check_existing_side(force_book, force_side):
-                force_book[force_side] = []
+        force_user_exists = False
+        for value in force_book.values():
+            if force_user in value:
+                force_user_exists = True
+                break
+        if force_side not in force_book.keys() and not force_user_exists:
+            force_book[force_side] = [force_user]
+        elif force_side in force_book.keys() and not force_user:
             force_book[force_side].append(force_user)
-        else:
+        elif force_user_exists:
             continue
     elif " -> " in command:
         force_user, force_side = command.split(" -> ")
-        for key, value in force_book.items():
-            for index in range(len(value)):
-                if force_book[key] == force_user:
-                    print(key)
+        for key in force_book.keys():
+            if force_user in force_book[key]:
+                force_book[key].remove(force_user)
+                break
+        if check_existing_side(force_book, force_side):
+            force_book[force_side] = []
+        force_book[force_side].append(force_user)
         print(f"{force_user} joins the {force_side} side!")
 
 for key, value in force_book.items():
