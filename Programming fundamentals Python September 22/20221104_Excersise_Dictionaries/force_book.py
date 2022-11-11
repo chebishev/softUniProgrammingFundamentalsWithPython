ad6f217da00df78de9f61c39c0b466370a1164f6
chebishev <atanas.chebishev@gmail.com>
2022-11-11 13:26:57 +0200

    11 of 12

--- Programming fundamentals Python September 22/20221104_Excersise_Dictionaries/force_book.py
+++ Programming fundamentals Python September 22/20221104_Excersise_Dictionaries/force_book.py
@@ -1,8 +1,3 @@
-def check_existing_side(dictionary, side):
-    if side not in dictionary.keys():
-        return True
-
-
 force_book = {}
 force_user_exists = False
 
@@ -12,35 +7,39 @@ while True:
     if command == "Lumpawaroo":
         break
 
-    if " | " in command:
+    if "|" in command:
+
         force_side, force_user = command.split(" | ")
         force_user_exists = False
+
         for value in force_book.values():
             if force_user in value:
                 force_user_exists = True
                 break
-        if force_side not in force_book.keys() and not force_user_exists:
-            force_book[force_side] = [force_user]
-        elif force_side in force_book.keys() and not force_user:
-            force_book[force_side].append(force_user)
-        elif force_user_exists:
-            continue
-    elif " -> " in command:
+
+        if not force_user_exists:
+            if force_side not in force_book.keys():
+                force_book[force_side] = [force_user]
+            else:
+                force_book[force_side].append(force_user)
+
+    elif "->" in command:
         force_user, force_side = command.split(" -> ")
-        for key in force_book.keys():
-            if force_user in force_book[key]:
-                force_book[key].remove(force_user)
+        for key, value in force_book.items():
+            if force_user in value:
+                force_book[key].pop(value.index(force_user))
                 break
-        if check_existing_side(force_book, force_side):
-            force_book[force_side] = []
-        force_book[force_side].append(force_user)
+        if force_side not in force_book.keys():
+            force_book[force_side] = [force_user]
+        else:
+            force_book[force_side].append(force_user)
         print(f"{force_user} joins the {force_side} side!")
 
 for key, value in force_book.items():
-    if len(force_book[key]) > 0:
+    if len(value) > 0:
         print(f"Side: {key}, Members: {len(value)}")
-        for index in range(len(value)):
-            print(f"! {value[index]}")
+        for user in value:
+            print(f"! {user}")
 
 # test inputs:
 
