2781abc82cdd9883971b27b16d0648dc89b3d5f4
chebishev <114015858+chebishev@users.noreply.github.com>
2022-11-12 14:48:24 +0200

    text processing problem 1

---
+++ Programming fundamentals Python September 22/20221111_Exercise_Text_Processing/valid_usernames.py
@@ -0,0 +1,25 @@
+words_list = input().split(", ")
+# consider strip() if judge is not merciful :)
+valid_symbol = False
+for username in words_list:
+    if 3 <= len(username) <= 16:
+          for character in range(len(username)):
+            valid_symbol = False
+            if username[character].isalnum() or \
+                    username[character] == "-" or \
+                    username[character] == "_":
+                valid_symbol = True
+            else:
+                valid_symbol = False
+                break
+        if valid_symbol:
+            if " " not in username:
+                print(username)
+    else:
+        continue
+
+# test inputs:
+
+# sh, too_long_username, !lleg@l ch@rs, jeffbutt
+
+# Jeff, john45, ab, cd, peter-ivanov, @smith
