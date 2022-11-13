504738b9200090dfc808a9cc305936c4bd524b17
chebishev <114015858+chebishev@users.noreply.github.com>
2022-11-13 18:07:21 +0200

    text processing problem Rage Quit still 85/100

--- Programming fundamentals Python September 22/20221111_Exercise_Text_Processing/rage_quit.py
+++ Programming fundamentals Python September 22/20221111_Exercise_Text_Processing/rage_quit.py
@@ -1,32 +1,50 @@
-quit_string = input()   # initial string
-quit_dict = {}  # dictionary whose keys will be string parts before number, and values - number (for multiplication)
-symbols = ""    # current symbols holder
-numbers = ""    # current numbers holder
-for character in quit_string:   # looped through every character in the string
+# initial string
+quit_string = input()
+
+# dictionary whose keys will be string parts before number, and values - number (for multiplication)
+quit_dict = {}
+
+symbols = ""  # current symbols holder
+numbers = ""  # current numbers holder
+
+# looping through every character in the string
+for character in quit_string:
     if character.isdigit():  # check if character is digit
-        if len(numbers) > 0:  # if there is data in numbers or symbols the digit is the second one
+        # if there is data in numbers or symbols variables, the digit is the second one
+        if len(numbers) == 1 and len(symbols):
             numbers += character  # adding the second (i hope) digit to numbers
             quit_dict[symbols] = int(numbers)  # adding the current symbols and the number as key/value in the dict
-            numbers = ""    # resetting the variables
+            numbers = ""  # resetting the variables
             symbols = ""
-        else:       # if no data in numbers: append the current digit
+        else:  # if no data in numbers: append the current digit
             numbers += character
-    else:
+
+    else:  # the character is not digit
         if len(numbers) == 0:  # if no digits in numbers is the time to push some characters
             symbols += character  # append the character to the string
-        else:              # if digit(s) in number we are already on characters so add the digits as value in the key
+        else:  # if digit(s) in number we are already on characters so add the digits as value in the key
             quit_dict[symbols] = int(numbers)
             numbers = ""  # reset the variable
-            symbols = character   # reset the variable and set new character as initial value
+            symbols = character  # reset the variable and set new character as initial value
+
 # Now we are out of the loop so if there is something valid in the variables - add it to the dictionary
-if len(symbols) and len(numbers):
+if len(symbols) or len(numbers):
     quit_dict[symbols] = int(numbers)
-unique_symbols = {}  # declaring empty dictionary in order to use all characters as unique keys
-final_string = ""  # this will be printed at the final
-for key in quit_dict.keys():  # looping through the keys and multiplicate them with the int(values)
+
+# declaring empty dictionary in order to use all characters as unique keys
+unique_symbols = {}
+
+# this will be printed at the final
+final_string = ""
+
+# looping through the keys and multiplicate them with the int(values)
+for key in quit_dict.keys():
     final_string += key * int(quit_dict[key])
-    for symbol in key: # looping through the characters of each key and add them as keys in the unique dictionary
+
+    # looping through the characters of each key and add them as keys in the unique dictionary
+    for symbol in key:
         unique_symbols[symbol.upper()] = 0  # obviously with value 0
+
 print(f"Unique symbols used: {len(unique_symbols)}")  # counting the unique values
 print(final_string.upper())  # printing the string with upper case
 
@@ -36,4 +54,3 @@ print(final_string.upper())  # printing the string with upper case
 # a3
 
 # aSd2&5s@1
-
