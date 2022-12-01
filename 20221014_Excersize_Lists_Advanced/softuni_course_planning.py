a74994d42082b6fad3c74763d50428163630f5fa
chebishev <atanas.chebishev@gmail.com>
2022-12-01 10:58:42 +0200

    lists advanced exercise - all completed

--- 20221014_Excersize_Lists_Advanced/softuni_course_planning.py
+++ 20221014_Excersize_Lists_Advanced/softuni_course_planning.py
@@ -9,6 +9,7 @@ while True:
     command = command.split(":")
     action = command[0]
     lesson_title = command[1]
+    exercise = lesson_title + "-Exercise"
 
     if action == "Add":
         if lesson_title not in initial_schedule:
@@ -17,42 +18,42 @@ while True:
     elif action == "Insert":
         index = int(command[2])
         if lesson_title not in initial_schedule:
-            if 0 <= index <= len(initial_schedule):
-                initial_schedule.insert(index, lesson_title)
+            initial_schedule.insert(index, lesson_title)
 
     elif action == "Remove":
         if lesson_title in initial_schedule:
             initial_schedule.remove(lesson_title)
-        exercise = lesson_title + "-Exercise"
         if exercise in initial_schedule:
             initial_schedule.remove(exercise)
 
     elif action == "Swap":
         new_lesson = command[2]
-        exercise = lesson_title + "-Exercise"
         new_exercise = new_lesson + "-Exercise"
         if lesson_title in initial_schedule and new_lesson in initial_schedule:
             index, new_index = initial_schedule.index(lesson_title), initial_schedule.index(new_lesson)
             initial_schedule[index], initial_schedule[new_index] = new_lesson, lesson_title
-            if initial_schedule[index + 1] == exercise and initial_schedule[new_index + 1] == new_exercise:
+            if exercise in initial_schedule and new_exercise in initial_schedule:
                 initial_schedule[index + 1], initial_schedule[new_index + 1] = new_exercise, exercise
-            elif initial_schedule[index + 1] == exercise:
-                initial_schedule.insert(index, initial_schedule.pop(index + 1))
-            elif new_index + 1 < len(initial_schedule):
-                if initial_schedule[new_index + 1] == new_exercise:
-                    initial_schedule.insert(index + 1, initial_schedule.pop(new_index + 1))
+            elif exercise in initial_schedule:
+                for position in range(len(initial_schedule)):
+                    if initial_schedule[position] == exercise:
+                        initial_schedule[position] = "0"
+                initial_schedule.insert(new_index + 1, exercise)
+            elif new_exercise in initial_schedule:
+                for position in range(len(initial_schedule)):
+                    if initial_schedule[position] == new_exercise:
+                        initial_schedule[position] = "0"
+                initial_schedule.insert(index + 1, new_exercise)
+            while "0" in initial_schedule:
+                initial_schedule.remove("0")
 
     elif action == "Exercise":
         if lesson_title in initial_schedule:
-            exercise = lesson_title + "-Exercise"
             exercise_index = initial_schedule.index(lesson_title) + 1
-            if initial_schedule[exercise_index] == exercise:
-                continue
-            else:
+            if exercise not in initial_schedule:
                 initial_schedule.insert(exercise_index, exercise)
         else:
             initial_schedule.append(lesson_title)
-            exercise = lesson_title + "-Exercise"
             initial_schedule.append(exercise)
 
 for index in range(len(initial_schedule)):
