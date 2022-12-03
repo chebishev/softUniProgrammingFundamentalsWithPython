8ec8fcb43aa24128e5f15eb84d142d896f2d18f1
chebishev <114015858+chebishev@users.noreply.github.com>
2022-12-03 13:34:28 +0200

    text processing more exercises

--- 20221111_Exercise_Text_Processing/treasure_finder.py
+++ 20221111_Exercise_Text_Processing/treasure_finder.py
@@ -33,9 +33,15 @@ while True:
     print(f"Found {treasure_type} at {coordinates}")
 
 
-# test input:
+# test inputs:
 
 # 1 2 1 3
 # ikegfp'jpne)bv=41P83X@
 # ujfufKt)Tkmyft'duEprsfjqbvfv=53V55XA
+# find
+
+# 1 4 2 5 3 2 1
+# Ulgwh"jt$ozfj!'kqqg(!bx"A3U237GC
+# tsojPqsf$(lrne'$CYfqpshksdvfT$>634O57YC
+# 'stj)>34W68Z@
 # find
