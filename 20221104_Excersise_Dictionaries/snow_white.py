b15f62d4354630e72ee7265fab58de97784732bf
chebishev <atanas.chebishev@gmail.com>
2022-12-17 09:41:46 +0200

    dictionaries more exercises problem 3 10/100

--- 20221104_Excersise_Dictionaries/snow_white.py
+++ 20221104_Excersise_Dictionaries/snow_white.py
@@ -15,9 +15,10 @@ while True:
         if physics > dwarf_dictionary[color][name]:
             dwarf_dictionary[color][name] = physics
 
-for dwarf, physics in sorted(dwarf_dictionary.items(), key=lambda x: len(x), reverse=True ):
-    for key, value in sorted(dwarf_dictionary[dwarf].items(), key=lambda x: (-(x[1]))):
-        print(f"({dwarf}) {key} <-> {value}")
+for color in sorted(dwarf_dictionary, key=lambda color: len(dwarf_dictionary[color]), reverse=True):
+    for dwarf, physics in sorted(dwarf_dictionary[color].items(), key= lambda x: -(x[1]), reverse=True):
+        print(dwarf, physics)
+
 
 # 10/100
 
