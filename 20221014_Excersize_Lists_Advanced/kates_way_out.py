9f003bd4c14648edc67c707d64f882e7ec82c74c
chebishev <atanas.chebishev@gmail.com>
2022-12-25 18:33:02 +0200

    lists advanced more exercises problem 3

--- 20221014_Excersize_Lists_Advanced/kates_way_out.py
+++ 20221014_Excersize_Lists_Advanced/kates_way_out.py
@@ -1,3 +1,18 @@
+def valid_position(matrix, row, col):
+    if 0 <= row < len(maze) and 0 <= col < len(maze[row]):
+        return True
+    return False
+
+def not_wall(maze, row, col):
+    if maze[row][cow] == " ":
+        return True
+    return False
+
+def exit():
+    if maze[row] == 0 or maze[row] == len(maze) - 1 and col == 0 or col == len(maze[row]) -1:
+        return True
+    return False
+
 maze_rows = int(input())
 #   # - wall
 #  " " - empty space
@@ -6,20 +21,39 @@ maze = []
 for row in range(maze_rows):
     current_row = [x for x in input()]
     maze.append(current_row)
-
+start_row = 0
+start_index = 0
 initial_index = 0
 initial_row = 0
+is_found = False
 for index in range(len(maze)):
+    if is_found:
+        break
     row = maze[index]
     if "k" in row:
         for position in row:
             if position == "k":
                 initial_index = row.index(position)
+                start_index = row.index(position)
                 initial_row = index
+                start_row = index
+                is_found = True
                 break
 
-print(initial_row)
-print(initial_index)
+while True:
+    if valid_position(maze, initial_row, initial_index -1) \
+        and not_wall(maze, initial_row, initial_index - 1):
+        maze[initial_row][initial_index - 1] = "k"
+        initial_index -= 1
+        
+    elif valid_position(maze, initial_row, initial_index + 1):
+        initial_index = initial_index + 1
+    elif valid_position(maze, initial_row-1, initial_index):
+        initial_row = initial_row - 1
+    elif valid_position(maze, initial_row + 1, initial_index):
+        initial_row = initial_row + 1
+    else:
+        break
 
 # test inputs:
 
